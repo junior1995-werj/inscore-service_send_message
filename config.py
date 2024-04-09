9adfdf91a11d5955739fe2cc8e794fb7c27058bd
Junior Werner <juninhowerner31@gmail.com>
2024-04-09 09:53:31 -0300

    modify variables ambiente

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
+import os
 from prettyconf import config
 
 class Settings:
 
-    SELLERS_API = config("SELLERS_API")
-    TOKEN_SELLERS = config("TOKEN_SELLERS")
-    SCHEDULE_API = config("SCHEDULE_API")
-    TOKEN_SCHEDULE = config("TOKEN_SCHEDULE")
+    SELLERS_API = os.environ["SELLERS_API"]
+    TOKEN_SELLERS = os.environ["TOKEN_SELLERS"]
+    SCHEDULE_API = os.environ["SCHEDULE_API"]
+    TOKEN_SCHEDULE = os.environ["TOKEN_SCHEDULE"]
 
 settings = Settings()
